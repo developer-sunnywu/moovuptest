720726d848b903dd0fa1233bbaf8fb83e0f7fc96
Sunny <wusunny2016@gmail.com>
2024-05-26 17:36:37 +0800

    Simple test

--- moovup-frontend/tests/unit/example.spec.js
+++ moovup-frontend/tests/unit/example.spec.js
@@ -1,12 +1,34 @@
-import { mount } from '@vue/test-utils'
+import { mount, shallowMount } from '@vue/test-utils'
+import People from '@/views/People.vue'
 
-const App = {
-  template: `
-    <div>Hello</div>
-  `
-}
+// describe("People.vue", () => {
+//   it("renders msg text", () => {
+//     const wrapper = mount(People);
+//     console.log(wrapper.html())
+//     // expect(wrapper.text()).toBe("new message");
+//   });
+// });
 
-test('App', () => {
-  const wrapper = mount(App)
-  console.log(wrapper.html())
-})
+// const App = {
+//   template: `
+//     <div>Hello</div>
+//   `
+// }
+
+// const app = People
+
+// test('App', () => {
+//   const wrapper = mount(app)
+//   console.log(wrapper)
+//   // expect(wrapper.html()).toContain('<div>Hello</div>')
+// })
+
+
+describe("Test to do list", () => {
+  it("Test to do 1", () => {
+    expect(1 + 1).toBe(2);
+  });
+  test("Test to do 2", () => {
+   expect(2 + 1).toBe(3);
+  });
+});
